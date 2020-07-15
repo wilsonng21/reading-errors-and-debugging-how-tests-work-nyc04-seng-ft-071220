3832c3e315bd47760e0f4df9548d61ed0a208acb
Wilson Ng <wilsongch92@gmail.com>
2020-07-15 17:58:40 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,11 +2,3 @@
 
 first_number = 30
 second_number = 2
-
-sum = first_number + second_number
-
-difference = first_number - second_number
-
-product = first_number * second_number
-
-quotient = first_number / second_number
